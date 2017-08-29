035eb0b760e3445787c3eacba34939e62f91df00
coldsice <coldsice@gmail.com>
2017-08-29 14:37:16 +0300

    fa icons added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TableLayoutModule } from './modules/table-layout/table-layout.module';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TableLayoutModule } from './modules/table-layout/table-layout.module';
   ],
   imports: [
     BrowserModule,
-    TableLayoutModule
+    TableLayoutModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/table-layout/table-layout.module.ts
+++ src/app/modules/table-layout/table-layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableLayoutComponent } from './components/table-layout/table-layout.component';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    Angular2FontawesomeModule
   ],
   declarations: [TableLayoutComponent],
   exports: [TableLayoutComponent]
